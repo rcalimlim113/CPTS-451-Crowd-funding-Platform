a470ae811fc1469fe9c00bef178b64ed97b214d4
notlucash <95841533+notlucash@users.noreply.github.com>
2026-03-15 16:42:20 -0700

    Reorder tables in schema.sql

--- schema.sql
+++ schema.sql
@@ -1,18 +1,3 @@
--- Campaigns table
-CREATE TABLE campaigns (
-    campaign_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    organizer_id INT NOT NULL REFERENCES users(user_id),
-    title VARCHAR(200) NOT NULL,
-    description VARCHAR(2000) NOT NULL,
-    funding_goal DECIMAL(12,2) NOT NULL CHECK (funding_goal > 0),
-    start_date DATE DEFAULT CURRENT_DATE NOT NULL,
-    end_date DATE NOT NULL,
-    status VARCHAR(20) DEFAULT 'pending' CHECK (status IN ('pending', 'active', 'completed', 'cancelled')),
-    is_approved  BOOLEAN DEFAULT FALSE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    CHECK (end_date > start_date)
-);
-
 -- Users Table
 CREATE TABLE Users (
     user_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
@@ -27,4 +12,19 @@ CREATE TABLE Users (
 
     CONSTRAINT chk_user_role 
     CHECK (role IN ('admin', 'user'))
+);
+
+-- Campaigns table
+CREATE TABLE campaigns (
+    campaign_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    organizer_id INT NOT NULL REFERENCES users(user_id),
+    title VARCHAR(200) NOT NULL,
+    description VARCHAR(2000) NOT NULL,
+    funding_goal DECIMAL(12,2) NOT NULL CHECK (funding_goal > 0),
+    start_date DATE DEFAULT CURRENT_DATE NOT NULL,
+    end_date DATE NOT NULL,
+    status VARCHAR(20) DEFAULT 'pending' CHECK (status IN ('pending', 'active', 'completed', 'cancelled')),
+    is_approved  BOOLEAN DEFAULT FALSE,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    CHECK (end_date > start_date)
 );
