d2bcc8c257bb75388a57ca7fa53fb8053df06777
johnnyWSU <johnny.huang@wsu.edu>
2026-04-11 00:17:52 -0700

    Connected schema to sqlite instance

--- README.md
+++ README.md
@@ -13,8 +13,9 @@ pip install Flask
 
 1. Install python 3.10 or above (3.12 used here)
 2. pip install Flask
-3. flask --app app run
-4. Follow the web link to see the website
+3. flask --app app run init-db
+4. flask --app app run --debug 
+5.  Follow the web link to see the website
 
 ### Additional 
 The program utilizes Jinja for templates
--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,36 @@
+import os
 from flask import Flask, request, render_template
-#from . import database
+from . import db
 
 #app = Flask(__name__)
 
-def create_app():
+def create_app(test_config=None):
     app = Flask(__name__)
-    
 
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY='dev', # this value should be randomized
+        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+    )
+
+    if test_config is None:
+        # load the instance config, if it exists, when not testing
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # load the test config if passed in
+        app.config.from_mapping(test_config)
+
+    # ensure the instance folder exists
+    os.makedirs(app.instance_path, exist_ok=True)
+
+    
+    # load hello.html page
     @app.route("/")
     def hello():
         return render_template('hello.html')
 
+    # load tempName.html page
     @app.route('/login', methods=['GET', 'POST'])
     def login():
         if request.method == 'POST':
@@ -18,13 +38,11 @@ def create_app():
             return f"Hello {name}, POST request received"
         return render_template('tempName.html')
 
-    if __name__ == '__main__':
-        app.run(debug=True)
-
 
     # import database 
-    from . import db
     db.init_app(app)
+
+
 
     return app
 
