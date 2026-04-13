909a18314fcddf804b65e6e61c6972c6fd8fc24c
johnnyWSU <johnny.huang@wsu.edu>
2026-04-12 21:19:17 -0700

    Removed user_donations table: user_id reference

--- app/schema.sql
+++ app/schema.sql
@@ -94,10 +94,9 @@ create table Donations(
 
 create table user_donations(
     donation_id INT PRIMARY KEY,
-    user_id INT,
-    payment_token VARCHAR(100) NOT NULL, -- NEEDS CHECK FOR EXISTING PAYMENT_TOKEN         -- No reference key, keeps donation while able to delete payment method
+    user_id INT NOT NULL, -- NEEDS CHECK FOR EXISTING USER      -- No reference key, keeps donation instance while able to delete a user
+    payment_token VARCHAR(100) NOT NULL, -- NEEDS CHECK FOR EXISTING PAYMENT_TOKEN         -- No reference key, keeps donation instance while able to delete payment method
     CONSTRAINT fk_userDonation_to_donationID FOREIGN KEY (donation_id) REFERENCES donations(donation_id), 
-    CONSTRAINT fk_userDonation_to_userID FOREIGN KEY (user_id) REFERENCES users(user_id)
 
 );
 
